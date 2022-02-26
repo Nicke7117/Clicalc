af92c64328dc15e43aecfd8dd6b1db29706be267
= <nikolas.syverod@gmail.com>
2022-02-26 19:42:06 +0200

    Create function to convert expression into reverse polish notation

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct Math {
     expression: String,
@@ -12,6 +14,7 @@ fn is_operator(token: &str) -> bool {
         "*" => true,
         "/" => true,
         "^" => true,
+        "%" => true,
         "(" => true,
         ")" => true,
         _ => panic!("Invalid operator: {}", token),
@@ -26,20 +29,18 @@ impl Math {
             reverse_polish_notation: Vec::new(),
         }
     }
-
     pub fn evaluate(&mut self) {
         self.split_into_tokens();
+        self.convert_to_reverse_polish_notation();
     }
 
     fn split_into_tokens(&mut self) {
         let mut token = String::new();
         for (i, c) in self.expression.chars().enumerate() {
             if c.is_numeric() {
+                token.push(c);
                 if i == self.expression.len() - 1 {
-                    token.push(c);
                     self.tokens.push(token.clone());
-                } else {
-                    token.push(c);
                 }
             } else if c.is_whitespace() {
                 if !token.is_empty() {
@@ -54,5 +55,41 @@ impl Math {
                 self.tokens.push(c.to_string());
             }
         }
+    }
+    fn convert_to_reverse_polish_notation(&mut self) {
+        let mut queue: Vec<String> = Vec::new();
+        let mut stack: Vec<String> = Vec::new();
+        let precedence =
+            HashMap::from([("+", 1), ("-", 1), ("*", 2), ("/", 2), ("^", 3), ("%", 3)]);
+        for token in self.tokens.iter() {
+            if token.parse::<f64>().is_ok() {
+                queue.push(token.to_string())
+            } else if token == "(" {
+                stack.push(token.to_string())
+            } else if token == ")" {
+                while let Some(popped) = stack.pop() {
+                    if popped == "(" {
+                        break;
+                    } else {
+                        queue.push(popped)
+                    }
+                }
+            } else if is_operator(token) {
+                while !stack.is_empty()
+                    && stack.last().unwrap() != "("
+                    && precedence.get(&*String::from(token)).unwrap()
+                        <= precedence
+                            .get(&*String::from(stack.last().unwrap()))
+                            .unwrap()
+                {
+                    queue.push(stack.pop().unwrap());
+                }
+                stack.push(token.to_string());
+            }
+        }
+        while !stack.is_empty() {
+            queue.push(stack.pop().unwrap());
+        }
+        self.reverse_polish_notation = queue.clone();
     }
 }
