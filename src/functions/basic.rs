098b03a723526c7e6f8893f29270cf2bf50f5cd3
= <nikolas.syverod@gmail.com>
2022-03-30 20:42:01 +0300

    Add math functions and handle division by zero error

--- src/functions/basic.rs
+++ src/functions/basic.rs
@@ -10,8 +10,12 @@ pub fn multiply(a: f64, b: f64) -> f64 {
     a * b
 }
 
-pub fn divide(a: f64, b: f64) -> f64 {
-    a / b
+pub fn divide(a: f64, b: f64) -> Result<f64, String> {
+    if b == 0.0 {
+        Err(String::from("Division by zero"))
+    } else {
+        Ok(a / b)
+    }
 }
 
 pub fn power(a: f64, b: f64) -> f64 {
@@ -20,4 +24,16 @@ pub fn power(a: f64, b: f64) -> f64 {
 
 pub fn modulus(a: f64, b: f64) -> f64 {
     a % b
+}
+
+pub fn sine(a: f64) -> f64 {
+    a.sin()
+}
+
+pub fn cosine(a: f64) -> f64 {
+    a.cos()
+}
+
+pub fn tangent(a: f64) -> f64 {
+    a.tan()
 }
